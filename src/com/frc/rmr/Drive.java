775dac19a9b82731956d22ab6fc8de386a95625c
Jackson <jackson@jackson-Aspire-5253.(none)>
2014-01-11 17:18:46 -0700

    Initial Commit YAY!

---
+++ src/com/frc/rmr/Drive.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.frc.rmr;
+
+import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author jackson
+ */
+public class Drive {
+    
+    public Drive(Watchdog watch, Joystick joy) {
+	
+    }
+    // you must check in with the watchdog during your operations in auto mode
+    public void autoDrive() {
+
+    }
+    // called repeatedly, don't call the watchdog
+    public void run() {
+	
+    }
+}
---
+++ src/com/frc/rmr/Vision.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.frc.rmr;
+
+import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author jackson
+ */
+public class Vision {
+	public Vision(Joystick joy) {
+		
+	}
+	// the distance array will have the first coordinate as the horizontal distance and the vertical distance as the second coordinate
+	// just put the values into this array once the values are determined.
+	public boolean isHot(int[] distances) {
+	    return false;
+	}
+	// update the display with whatever we need it to say, do not update the watchdog
+	public void run() {
+
+	}
+}
