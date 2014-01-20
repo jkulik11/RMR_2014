971f8204353566f1968b7a9d4b75ffefebc98966
Drive Station Laptop <danielmercer1@gmail.com>
2014-01-20 14:24:17 -0700

    Fixed the PID Controller so that it runs

--- src/com/frc/rmr/Drive.java
+++ src/com/frc/rmr/Drive.java
@@ -1,4 +1,7 @@
-/*
+
+
+
+/*/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -10,21 +13,23 @@ import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
 
-/**
+/*
  *
  * @author jackson
  */
+
 public class Drive {
-    
-    private static final int MAX_SPEED = 100;
+    //jaguar input from -1 to 1
+    private static final double MAX_SPEED = .4;
     private static final double WHEEL_RADIUS = 3.0;
     private static final int TICKS_PER_ROTATION = 360;
     
-    private static final double KP = 1;
+    private static final double KP = .3;
     private static final double KI = 0;
     private static final double KD = 0;
-    private static final double KF = 0;
+    private static final double KF = .3;
     
     private static final int RIGHT_MOTOR_PORT = 2;
     private static final int[] RIGHT_ENCODER_CHANNELS = {1, 2};
@@ -42,7 +47,8 @@ public class Drive {
     private Watchdog watch;
     private PIDController leftController, rightController;
     private boolean[] buttonWasPressed;
-    
+    private Jaguar rightMotor;
+    private Jaguar leftMotor;
     
     public Drive(Watchdog watch, Joystick joy) {
 	this.watch = watch;
@@ -52,11 +58,13 @@ public class Drive {
 	for (int i = 0; i < 10; ++i) { buttonWasPressed[i] = false; }
 	
 	// create the right pid motor and controller
-	Jaguar rightMotor = new Jaguar(RIGHT_MOTOR_PORT);
+	rightMotor = new Jaguar(RIGHT_MOTOR_PORT);
 	Encoder rightEncoder = new Encoder(RIGHT_ENCODER_CHANNELS[0], RIGHT_ENCODER_CHANNELS[1]);
 	rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
 	rightEncoder.start();
 	rightEncoder.reset();
+	// changing this and leftEncoder's setPIDSource will make it run on teleop or auto
+	rightEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kRate);
 	
 	rightController = new PIDController(KP, KI, KD, KF, rightEncoder, rightMotor);
 	rightController.enable();
@@ -64,11 +72,13 @@ public class Drive {
 	rightController.setOutputRange(-MAX_SPEED, MAX_SPEED);
 	
 	// create the left pid motor and controller
-	Jaguar leftMotor = new Jaguar(LEFT_MOTOR_PORT);
+	leftMotor = new Jaguar(LEFT_MOTOR_PORT);
 	Encoder leftEncoder = new Encoder(LEFT_ENCODER_CHANNELS[0], LEFT_ENCODER_CHANNELS[1]);
 	leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
 	leftEncoder.start();
 	leftEncoder.reset();
+	// changing this and rightEncoder's setPIDSource will make it run on teleop or auto
+	leftEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kRate);
 	
 	leftController = new PIDController(KP, KI, KD, KF, leftEncoder, leftMotor);
 	leftController.enable();
@@ -76,13 +86,17 @@ public class Drive {
 	leftController.setOutputRange(-MAX_SPEED, MAX_SPEED);
     }
     // you must check in with the watchdog during your operations in auto mode
+
     public void autoDrive() {
-	/*// create the right pid motor and controller
+	/*
+	// create the right pid motor and controller
 	Jaguar rightMotor = new Jaguar(RIGHT_MOTOR_PORT);
 	Encoder rightEncoder = new Encoder(RIGHT_ENCODER_CHANNELS[0], RIGHT_ENCODER_CHANNELS[1]);
+	
 	rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
 	rightEncoder.start();
 	rightEncoder.reset();
+	rightEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
 	PIDController rightController = new PIDController(KP, KI, KD, rightEncoder, rightMotor);
 	rightController.enable();
 	
@@ -92,14 +106,17 @@ public class Drive {
 	leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
 	leftEncoder.start();
 	leftEncoder.reset();
+	leftEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
 	PIDController leftController = new PIDController(KP, KI, KD, leftEncoder, leftMotor);
-	leftController.enable();*/
+	leftController.enable();
 	
 	// TODO: missing actual distance code and control
 	
 	// TODO: remove
-	leftController.setSetpoint(1000);
-	rightController.setSetpoint(1000);
+*/
+	
+	// leftController.setSetpoint(1000);
+	// rightController.setSetpoint(1000);
 	
 	watch.feed();
     }
@@ -121,9 +138,13 @@ public class Drive {
     
     // called repeatedly, don't call the watchdog
     public void run() {
+	
 	if (Math.abs(joy.getRawAxis(XboxMap.LeftJoyVert)) >= .05) {
-	    leftController.setSetpoint(joy.getRawAxis(XboxMap.LeftJoyVert));
-	    rightController.setSetpoint(joy.getRawAxis(XboxMap.LeftJoyVert));
+	    leftController.setSetpoint(.5); //joy.getRawAxis(XboxMap.LeftJoyVert));
+	    rightController.setSetpoint(.5);  //joy.getRawAxis(XboxMap.LeftJoyVert));
+	} else {
+	    leftController.setSetpoint(0);
+	    rightController.setSetpoint(0);
 	}
 	
 	double tuningSize = .1;
@@ -176,3 +197,79 @@ public class Drive {
 	// TODO: tuning from the controller
     }
 }
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+package com.frc.rmr;
+
+import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Encoder;
+
+/**
+ *
+ * @author jackson
+public class Drive {
+    
+	public static final double DISTANCE_PP = 6.0 * Math.PI / 360.0;
+	public static final int JAGL = 1;
+	public static final int JAGR = 2;
+	public static final int ENL1 = 3;
+	public static final int ENL2 = 4;
+	public static final int ENR1 = 1;
+	public static final int ENR2 = 2; 
+	
+	private double p = .1;
+	private double i = 0;
+	private double d = 0;
+	private PIDController pidL, pidR;
+	private Jaguar leftD, rightD;
+	private Encoder leftE, rightE;
+	private Joystick joy;
+	private Watchdog watch;
+
+	public Drive(Watchdog watch, Joystick joy) {
+		this.watch = watch;
+		this.joy = joy;
+		leftD = new Jaguar(JAGL);
+		rightD = new Jaguar(JAGR);
+		leftE = new Encoder(ENL1, ENL2);
+		rightE = new Encoder(ENR1, ENR2);
+		leftE.setDistancePerPulse(DISTANCE_PP);
+		rightE.setDistancePerPulse(DISTANCE_PP);
+		leftE.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
+		rightE.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
+		pidL = new PIDController(p, i, d, leftE, leftD);
+		pidL.setPercentTolerance(.02);
+		pidL.setContinuous(true);
+		pidR = new PIDController(p, i, d, rightE, rightD);
+		pidR.setPercentTolerance(.02);
+		pidR.setContinuous(true);
+	}
+	// you must check in with the watchdog during your operations in auto mode
+	/*
+	public void autoDrive() {
+		System.out.println("entered");
+		   /*
+		pidL.enable();
+		pidR.enable();
+		pidL.setSetpoint(100.0);
+		pidR.setSetpoint(100.0);
+			   
+		rightD.set(.3);
+		leftD.set(.3);
+		while (true) {
+		    watch.feed();
+		}
+	}
+	// called repeatedly, don't call the watchdog
+	public void run() {
+
+	}
+}
+*/
---
+++ src/java/frc/drive/MainDrive.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java.frc.drive;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDController;
+/**
+ *
+ * @author ROBOTics
+ */
+/*
+
+private Watchdog watch;
+private Joystick joy;
+
+public class MainDrive(Watchdog watch, Joystick joy) {
+    	this.watch = watch;
+	
+	this.joy = joy;
+	buttonWasPressed = new boolean[10];
+	for (int i = 0; i < 10; ++i) { buttonWasPressed[i] = false; }
+	
+	// create the right pid motor and controller
+	rightMotor = new Jaguar(RIGHT_MOTOR_PORT);
+	Encoder rightEncoder = new Encoder(RIGHT_ENCODER_CHANNELS[0], RIGHT_ENCODER_CHANNELS[1]);
+	rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+	rightEncoder.start();
+	rightEncoder.reset();
+	rightEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
+	
+	rightController = new PIDController(KP, KI, KD, KF, rightEncoder, rightMotor);
+	rightController.enable();
+	rightController.setInputRange(-MAX_SPEED, MAX_SPEED);
+	rightController.setOutputRange(-MAX_SPEED, MAX_SPEED);
+	
+	// create the left pid motor and controller
+	leftMotor = new Jaguar(LEFT_MOTOR_PORT);
+	Encoder leftEncoder = new Encoder(LEFT_ENCODER_CHANNELS[0], LEFT_ENCODER_CHANNELS[1]);
+	leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+	leftEncoder.start();
+	leftEncoder.reset();
+	leftEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
+	
+	leftController = new PIDController(KP, KI, KD, KF, leftEncoder, leftMotor);
+	leftController.enable();
+	leftController.setInputRange(-MAX_SPEED, MAX_SPEED);
+	leftController.setOutputRange(-MAX_SPEED, MAX_SPEED);
+}
+*/
